a40a24e87ac9b0ce7776ffdcb142bf18e4bdf275
Aslam110-max <ma6237687@gmail.com>
2024-04-17 14:09:35 +0530

    updated

--- Yarl Vibe api/Controllers/YarlVibeController.cs
+++ Yarl Vibe api/Controllers/YarlVibeController.cs
@@ -52,6 +52,7 @@ namespace Yarl_Vibe_api.Controllers
                     table.Load(myReader);
                     myReader.Close();
                     myCon.Close();
+                    //chn
                 }
             }
             return new JsonResult("Added Successfully!");
