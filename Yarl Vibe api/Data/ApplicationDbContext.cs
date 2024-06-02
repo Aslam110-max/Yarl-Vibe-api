2915340a7e0efeeedf6660fbbcf70f5a4a24fe11
DarmilaT <darmila.t02gh@gmail.com>
2024-06-02 20:48:53 +0530

    Add ApplicationDbContext

---
+++ Yarl Vibe api/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿namespace Yarl_Vibe_api.Data
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        
+    }
+}
